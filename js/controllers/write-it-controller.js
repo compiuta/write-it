80e996f9443f2ce8cc9d84fb8463275d3f9372e4
riccardou <christianr@rocketmail.com>
2020-01-29 13:48:33 -0800

    create addNote and add init items

--- js/controllers/write-it-controller.js
+++ js/controllers/write-it-controller.js
@@ -1,7 +1,37 @@
 (function(window) {
     let writeItController = {
+        addNote: function(e) {
+            e.preventDefault();
+            let noteObject = {
+                noteTitle: app.writeItView.createNoteTitle.value,
+                noteBody: app.writeItView.createNoteTextArea.value,
+                noteCreatedDate: new Date()
+            }
+
+            if(app.writeItView.createNoteTitle.value.replace(/ /g, '') !== '' && app.writeItView.createNoteTextArea.value.replace(/ /g, '') !== '') {
+                app.writeItModel.setData(noteObject);
+
+                app.writeItView.createNoteTitle.value = '';
+                app.writeItView.createNoteTextArea.value = '';
+
+                app.writeItView.showNoteAlert('Message Saved Successfully!');
+            } else {
+                if(app.writeItView.createNoteTitle.value.replace(/ /g, '') === '') {
+                    app.writeItView.createNoteTitle.value = '';
+                }
+
+                if(app.writeItView.createNoteTextArea.value.replace(/ /g, '') === '') {
+                    app.writeItView.createNoteTextArea.value = '';
+                }
+
+                app.writeItView.showNoteAlert('Please Fill Out All Fields');
+            }
+        },
         init: function() {
-            window.app.writeItView.init();
+            app.writeItView.init();
+            app.writeItModel.init();
+
+            app.writeItView.createNoteSubmit.addEventListener('click', this.addNote);
         }
     }
     
