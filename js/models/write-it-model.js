0b4975087d3300ad97d77e0285f45a957c5d3a7b
riccardou <christianr@rocketmail.com>
2020-02-01 06:11:34 -0800

    refactor code to allow for deletion of notes

--- js/models/write-it-model.js
+++ js/models/write-it-model.js
@@ -1,23 +1,46 @@
 (function(window){
     let writeItModel = {
         allNotes: {},
+        noteCount: 0,
         months: ["January", "February", "March", "April", "May", "June", "July",
          "August", "September", "October", "November", "December"],
         getData: function() {
-            for(let i = 0; i < Object.keys(localStorage).length; i++) {
-                this.allNotes['note' + i] = JSON.parse(localStorage.getItem('note' + i));
-            }
+            let data = JSON.parse(localStorage.getItem('allNotes'));
+            let notesCreated = JSON.parse(localStorage.getItem('noteCount'));
+        
+            this.allNotes = data;
+            this.noteCount = notesCreated;
         },
         setData: function(noteObject) {
-            localStorage.setItem('note' + (Object.keys(this.allNotes).length), JSON.stringify(noteObject));
+            let data = JSON.parse(localStorage.getItem('allNotes'));
+            let notesCreated = JSON.parse(localStorage.getItem('noteCount'));
+           
+            data['note' + notesCreated] = noteObject;
+            notesCreated++;
+
+            localStorage.setItem('allNotes', JSON.stringify(data));
+            localStorage.setItem('noteCount', JSON.stringify(notesCreated));
+
+            this.getData();
+        },
+        deleteData: function(noteToDelete) {
+            let data = JSON.parse(localStorage.getItem('allNotes'));
+           
+            delete data[noteToDelete];
+            localStorage.setItem('allNotes', JSON.stringify(data));
 
             this.getData();
         },
         init: function() {
+            if(!(JSON.parse(localStorage.getItem('allNotes')))){
+                localStorage.setItem('allNotes', JSON.stringify({}));
+                localStorage.setItem('noteCount', JSON.stringify(0));
+            }
+
             this.getData();
         }
     }
 
     window.app = window.app || {};
     window.app.writeItModel = writeItModel;
-})(window);
+})(window)
