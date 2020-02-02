e0fad7f4846d97e7979a90726a1129f9a5c581e0
riccardou <christianr@rocketmail.com>
2020-02-02 09:05:35 -0800

    refactor setData and init functions

--- js/models/write-it-model.js
+++ js/models/write-it-model.js
@@ -5,21 +5,23 @@
         months: ["January", "February", "March", "April", "May", "June", "July",
          "August", "September", "October", "November", "December"],
         getData: function() {
-            let data = JSON.parse(localStorage.getItem('allNotes'));
-            let notesCreated = JSON.parse(localStorage.getItem('noteCount'));
-        
-            this.allNotes = data;
-            this.noteCount = notesCreated;
+            this.allNotes = JSON.parse(localStorage.getItem('allNotes'));;
+            this.noteCount = JSON.parse(localStorage.getItem('noteCount'));;
         },
         setData: function(noteObject) {
-            let data = JSON.parse(localStorage.getItem('allNotes'));
-            let notesCreated = JSON.parse(localStorage.getItem('noteCount'));
-           
-            data['note' + notesCreated] = noteObject;
-            notesCreated++;
+            if(JSON.parse(localStorage.getItem('allNotes')) && noteObject){
+                let data = JSON.parse(localStorage.getItem('allNotes'));
+                let notesCreated = JSON.parse(localStorage.getItem('noteCount'));
+            
+                data['note' + notesCreated] = noteObject;
+                notesCreated++;
 
-            localStorage.setItem('allNotes', JSON.stringify(data));
-            localStorage.setItem('noteCount', JSON.stringify(notesCreated));
+                localStorage.setItem('allNotes', JSON.stringify(data));
+                localStorage.setItem('noteCount', JSON.stringify(notesCreated));
+            } else if(!(JSON.parse(localStorage.getItem('allNotes')))) {
+                localStorage.setItem('allNotes', JSON.stringify({}));
+                localStorage.setItem('noteCount', JSON.stringify(0));
+            }
 
             this.getData();
         },
@@ -32,12 +34,7 @@
             this.getData();
         },
         init: function() {
-            if(!(JSON.parse(localStorage.getItem('allNotes')))){
-                localStorage.setItem('allNotes', JSON.stringify({}));
-                localStorage.setItem('noteCount', JSON.stringify(0));
-            }
-
-            this.getData();
+            this.setData();
         }
     }
 
